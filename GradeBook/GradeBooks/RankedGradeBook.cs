4caf7cfc18a56efc067253360482462863048a72
Szymon Gałaś <szymon.galas@microsoft.wsei.edu.pl>
2022-03-31 23:46:19 +0200

    Commit 20/24

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,69 @@
-﻿using System;
+﻿using GradeBook.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook
+    class RankedGradeBook : BaseGradeBook
     {
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            if (averageGrade > 80)
+            {
+                return 'A';
+            }
+            else if (averageGrade > 60)
+            {
+                return 'B';
+            }
+            else if (averageGrade > 40)
+            {
+                return 'C';
+            }
+            else if (averageGrade > 20)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
+        }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count<5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+
     }
 }
--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,15 +34,32 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 4)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).");
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            bool IsWeighted = Convert.ToBoolean(parts[3]);
+            StandardGradeBook gradeBook = new StandardGradeBook(name, IsWeighted);
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
+
+            if (parts[2] == "standard")
+            {
+                name = parts[2];
+                StandardGradeBook standardGradeBook = new StandardGradeBook(name, IsWeighted);
+            }
+            else if (parts[2] == "ranked")
+            {
+                name = parts[2];
+                RankedGradeBook rankedGradeBook = new RankedGradeBook(name, IsWeighted);
+            }
+            else
+            {
+                Console.WriteLine("is not a supported type of gradebook, please try again");
+                
+            }
         }
 
         public static void LoadCommand(string command)
@@ -67,7 +84,7 @@ namespace GradeBook.UserInterfaces
             Console.WriteLine();
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' - Creates a new gradebook where 'Name' is the name of the gradebook.");
+            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
